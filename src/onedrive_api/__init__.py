69db1ed13bdc9c30e5057bdcfdeff3beb31bb12c
Jaemin Kim <odumag99@naver.com>
2025-04-03 20:39:41 +0900

    login Redirect URL 작성

--- src/onedrive_api/__init__.py
+++ src/onedrive_api/__init__.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import dotenv
+import os
+from urllib.parse import quote
 
+dotenv.load_dotenv()
 app = FastAPI()
 
-@app.get("/callback")
-async def callback()
+@app.get("/login")
+async def login():
+    scope = quote("offline_access files.readwrite.all")
+    return RedirectResponse(
+        url = f"""https://login.microsoftonline.com/common/oauth2/v2,0/authorize?
+client_id={os.getenv("MICROSOFT_APP_CLIENT_ID")}
+&response_type=code
+&redirect_uri=http://{os.getenv("FRONT_URL")}/oauth2/callback
+&response_model=query
+&scope={scope}"""
+    )
