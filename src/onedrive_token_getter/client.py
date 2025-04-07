17025ee4409c882b993155ec06ea13ece3f6931f
Jaemin Kim <odumag99@naver.com>
2025-04-07 20:15:03 +0900

    client.get_code 작성

--- src/onedrive_token_getter/client.py
+++ src/onedrive_token_getter/client.py
@@ -1,9 +1,11 @@
 import httpx
 import dotenv
 import os
+from typing import Tuple
 from .common import OAUTH_REDIRECT_URI
 
 dotenv.load_dotenv()
+TOKEN_GETTER_SERVER_URI = f"https://{os.getenv("FRONT_URL")}"
 
 async def get_access_refresh_token(code: str):
     url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
@@ -34,3 +36,18 @@ async def get_access_refresh_token(code: str):
         raise Exception(res)
 
     return res_json["access_token"], res_json["refresh_token"]
+
+async def get_code() -> str: 
+    async with httpx.AsyncClient() as client:
+        res = await client.get(
+            url = TOKEN_GETTER_SERVER_URI + f"/code"
+        )
+
+        res.raise_for_status()
+
+        res_json = res.json()
+
+        if not res_json["code"]:
+            raise Exception(res)
+        
+        return res_json["code"]
