235afacb11bf9d34051eeb3a657740065061bb4f
Jaemin Kim <odumag99@naver.com>
2025-04-07 16:15:31 +0900

    run.py 추가

---
+++ run.py
@@ -0,0 +1,12 @@
+import src.onedrive_api as fastapi
+import uvicorn
+
+app = fastapi.app
+
+if __name__ == "__main__":
+    uvicorn.run(app,
+                port=8000,
+                reload=True, 
+                ssl_certfile="./openssl-certs/cert.pem", 
+                ssl_keyfile="./openssl-certs/key.pem"
+    )
