f6a6adce246e68a370adb0aefb06ecb33245fd02
Jaemin Kim <odumag99@naver.com>
2025-04-07 19:59:44 +0900

    app -> server=FastAPI()

--- src/onedrive_token_getter/__init__.py
+++ src/onedrive_token_getter/__init__.py
@@ -1,10 +1,7 @@
-import onedrive_token_getter.server as fastapi
 import uvicorn
 
-app = fastapi.server
-
 if __name__ == "__main__":
-    uvicorn.run(app="src.onedrive_token_getter:app",
+    uvicorn.run(app="src.onedrive_token_getter:server",
                 port=8000,
                 reload=True, 
                 ssl_certfile="./openssl-certs/cert.pem", 
--- src/onedrive_token_getter/server.py
+++ src/onedrive_token_getter/server.py
@@ -8,13 +8,13 @@ from .client import get_access_refresh_token
 
 dotenv.load_dotenv()
 
-app = FastAPI()
+server = FastAPI()
 CODE = os.getenv("ONEDRIVE_AUTHENTICATION_CODE")
 ACCESS_TOKEN = os.getenv("ONEDRIVE_ACCESS_TOKEN")
 REFRESH_TOKEN = os.getenv("ONEDRIVE_REFRESH_TOKEN")
 
 
-@app.get("/login")
+@server.get("/login")
 async def login():
     redirect_uri = quote(OAUTH_REDIRECT_URI)
     scope = quote("offline_access files.readwrite.all")
@@ -27,18 +27,18 @@ client_id={os.getenv("MICROSOFT_APP_CLIENT_ID")}
 &scope={scope}"""
     )
 
-@app.get("/oauth2/callback", status_code=200)
+@server.get("/oauth2/callback", status_code=200)
 async def set_code(code: str):
     CODE = code
     return Response(status_code=status.HTTP_200_OK)
 
-@app.get("/code")
+@server.get("/code")
 async def get_code():
     if not CODE:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
     return {"code" : CODE}
 
-@app.get("/tokens")
+@server.get("/tokens")
 async def get_tokens():
     if ACCESS_TOKEN is None and REFRESH_TOKEN is None:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
@@ -46,14 +46,14 @@ async def get_tokens():
     return {"access_token" : ACCESS_TOKEN if ACCESS_TOKEN else None,
             "refresh_token" : REFRESH_TOKEN if REFRESH_TOKEN else None}
 
-@app.get("/tokens/access_token")
+@server.get("/tokens/access_token")
 async def get_access_token():
     if ACCESS_TOKEN is None:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
 
     return {"access_token" : ACCESS_TOKEN}
 
-@app.get("tokens/refresh_token")
+@server.get("tokens/refresh_token")
 async def get_refresh_token():
     if REFRESH_TOKEN is None:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
